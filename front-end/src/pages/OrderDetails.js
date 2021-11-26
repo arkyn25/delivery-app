0610e24f5db0e083c783662c4619001ac62ab7a8
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-11-26 16:02:19 -0300

    fix lint

--- front-end/src/pages/OrderDetails.js
+++ front-end/src/pages/OrderDetails.js
@@ -1,51 +1,11 @@
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
-import api from '../services';
-
-export default function OrderDetails() {
-  const [productsOrder, setProductsOrder] = useState([]);
-  // const [idUrl, setIdUrl] = useState('');
-  // const getSaleId = () => {
-  // setIdUrl(url[url.length - 1]);
-  // const url = useLocation().pathname.split('/');
-  //   return saleId;
-  const { id } = useParams();
-  // };
-  useEffect(() => {
-    (async () => {
-      console.log(id);
-      const products = await api.getAllSalesProductsbySaleId(id);
-      setProductsOrder(products);
-    })();
-  }, [id]);
-  return (
-    <div>
-      {/* { console.log(url)} */}
-      hello galera
-      {/* {productsOrder.map((value) => console.log(value))} */}
-      {console.log(productsOrder)}
-      {/* {console.log(id)} */}
-      {/* {console.log(saleId)} */}
-    </div>
-  );
-}
-// OrderDetails.propTypes = {
-//   totalPrice: PropTypes.number.isRequired,
-// };
-=======
-
 import React from 'react';
 import NavBar from '../components/navBar';
 import OrderTable from '../components/orderTable';
 
 export default function OrderDetails() {
-
   return (
     <>
       <NavBar />
-
       <p>Detalhe do pedido</p>
       <span data-testid="customer_order_details__element-order-details-label-order-id">
         id
@@ -68,4 +28,3 @@ export default function OrderDetails() {
     </>
   );
 }
->>>>>>> dd4a7a99be1c13207fc41302b84dcaf09cb71a99

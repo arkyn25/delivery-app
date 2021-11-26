4640bab6cc6815efaffc87fa983a9141b627f215
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-11-26 16:40:43 -0300

    update: req 41,42

--- back-end/src/api/controllers/registerController.js
+++ back-end/src/api/controllers/registerController.js
@@ -1,6 +1,14 @@
 const rescue = require('express-rescue');
-const registerService = require('../services/registerService');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const fs = require('fs');
 const createToken = require('../middlewares/createToken');
+const registerService = require('../services/registerService');
+
+const secret = fs
+  .readFileSync(
+    path.join(`${__dirname}../../../../jwt.evaluation.key`), { encoding: 'utf-8' },
+  ).trim();
 
 const registerUser = rescue(async (req, res) => {
   const { name, email, password, role = 'customer' } = req.body;
@@ -11,6 +19,22 @@ const registerUser = rescue(async (req, res) => {
   return res.status(201).json({ id: newUser.id, name, email, role, token });
 });
 
+const registerByAdmin = rescue(async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const adminToken = req.headers.authorization;
+  const decoded = jwt.verify(adminToken, secret);
+
+  if (decoded.data.role === 'administrator') {
+    const newUser = await registerService.registerUser(name, email, password, role); 
+    const { error } = newUser;
+    if (error) return res.status(error.status).json(error.message);
+    const token = await createToken({ id: newUser.id, name, email, role });
+    return res.status(201).json({ id: newUser.id, name, email, role, token });
+  }
+  return res.status(400).json({ message: 'ação inválida' });
+});
+
 module.exports = {
   registerUser,
+  registerByAdmin,
 };

1cc2e0906135abaa0eeda9e145838c296581f918
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-11-26 20:19:31 -0300

    fix: import React

--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect, useState } from 'react/cjs/react.development';
+import React, { useEffect, useState } from 'react';
 // import api from '../services';
 import DetailsAddress from '../components/detailsAddress';
 

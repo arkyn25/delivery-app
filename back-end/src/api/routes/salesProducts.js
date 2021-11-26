9cdce18aaefebffd1ccd38d9fb01efb8c3a06d15
Felippe017 <felippecorrea2010@gmail.com>
2021-11-26 14:55:29 -0300

    Consertando rota no arquivo salesProducts no back-end

--- back-end/src/api/routes/salesProducts.js
+++ back-end/src/api/routes/salesProducts.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const salesProductsController = require('../controllers/salesProducts');
 
 router.get('/checkout', salesProductsController.getAll);
-router.get('/customer/orders:id', salesProductsController.getAllByUserId);
+router.get('/customer/orders:id', salesProductsController.getAllSalesProductsBySeleId);
 router.post('/salesProducts', salesProductsController.createSalesProducts);
 router.get('/salesProducts', salesProductsController.getAllSalesProductsBySeleId);
 
